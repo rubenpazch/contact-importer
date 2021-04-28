e33efad11f8cfc064422af9482cd74c2553735db
Ruben Paz Chuspe <rubenpazchuspe@outlook.com>
2021-04-28 12:48:21 -0500

    add user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :email, uniqueness: true
+  validates_format_of :email, with: /@/
+  
+  has_many :contacts
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.delete_all
+
+u1 = User.create(email: 'rubena@gmail.com', password: '123456')
+u2 = User.create(email: 'rubenb@gmail.com', password: '123456')
