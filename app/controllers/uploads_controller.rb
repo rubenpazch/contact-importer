206d0d871f3f806564eabbc6377196302ee6e8dc
Ruben Paz Chuspe <rubenpazchuspe@outlook.com>
2021-04-28 12:35:12 -0500

    add contact migration

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -1,4 +1,5 @@
 class UploadsController < ApplicationController
+  before_action :authenticate_user!
   def index 
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_28_153410) do
+ActiveRecord::Schema.define(version: 2021_04_28_173425) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "name"
+    t.date "date_of_birth"
+    t.string "phone"
+    t.string "address"
+    t.string "credit_card"
+    t.string "franchise"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
