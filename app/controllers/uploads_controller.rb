bb98c5bd2db8fec6f026d4b39e2d91ca8e7a152d
Ruben Paz Chuspe <rubenpazchuspe@outlook.com>
2021-04-28 20:26:53 -0500

    add upload improt class

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -2,4 +2,9 @@ class UploadsController < ApplicationController
   before_action :authenticate_user!
   def index 
   end
+
+  def import 
+    Upload.import(params[:file])
+    redirect_to root_url, notice: "Activity data imported!"
+  end
 end
--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,2 +1,9 @@
 class Upload < ApplicationRecord
+  require 'csv'
+
+  def self.import(file)
+    CSV.foreach(file.path, headers: true) do |row|
+      Contact.create! row.to_hash
+    end
+  end
 end
